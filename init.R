48cd770750ab2f0ae1b22968c8256e59e1bf4c4f
ahmetmenevse <ahmetmenevse26@gmail.com>
2024-07-18 10:35:05 +0800

    Updated init.R to install required packages

--- init.R
+++ init.R
@@ -1,10 +1,8 @@
-# Example R code to install packages if not already installed
-
 my_packages = c('shiny', 'dplyr', 'randomForest', 'e1071', 'pROC', 'ggplot2', 'caret', 'shinythemes', 'VIM', 'DT')
 
 install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+  if (!p %in% rownames(installed.packages())) {
+    install.packages(p, repos='http://cran.rstudio.com/')
   }
 }
 
