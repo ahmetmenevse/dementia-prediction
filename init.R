85955e400f45b337bd4a93e3d4e30ca4b0114efa
ahmetmenevse <ahmetmenevse26@gmail.com>
2024-07-18 13:07:33 +0800

    Fix missing VIM package issue

--- init.R
+++ init.R
@@ -3,7 +3,22 @@ my_packages = c('shiny', 'dplyr', 'randomForest', 'e1071', 'pROC', 'ggplot2', 'c
 install_if_missing = function(p) {
   if (!p %in% rownames(installed.packages())) {
     install.packages(p, repos='http://cran.rstudio.com/')
+  } else {
+    message(paste("Package", p, "is already installed."))
   }
 }
 
-invisible(sapply(my_packages, install_if_missing))
+install_package_with_retry = function(p) {
+  tryCatch(
+    {
+      install_if_missing(p)
+    },
+    error = function(e) {
+      message(paste("Error installing package:", p))
+      message("Retrying...")
+      install.packages(p, repos='http://cran.rstudio.com/')
+    }
+  )
+}
+
+invisible(sapply(my_packages, install_package_with_retry))
